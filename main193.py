31e39a7faecdfdb8777c152823a701a032cdd6f9
Salohiddin <nozimboevs@gmail.com>
2024-02-29 09:30:03 +0500

    sixth commit

---
+++ main193.py
@@ -0,0 +1,35 @@
+# # text = 'Hello World'
+# # print(hex(id(text)))
+# #
+# # text = 'Hello World 2'
+# # print(hex(id(text)))
+#
+# #
+# # text = 'Hello World'
+# # print(hex(id(text)))
+# #
+# # text += 'Salom Dunyo'
+# # print(hex(id(text)))
+# #
+# #
+# # num1 = 45
+#
+# cars = ['Range Rover', 'Renault', 'Wolksvagen', 'BYD', 'Mercedes', 'Lincoln', 'BMW']
+#
+# cars.append('Chevrolet')
+# cars.extend(['Ford', 'Rollce Royce'])
+# cars.insert(3, 'Toyota')
+#
+# cars.remove('Range Rover')
+# cars.pop(0)           #id
+#
+#
+# print(cars)
+#
+#
+#
+#
+#
+#
+#
+#
